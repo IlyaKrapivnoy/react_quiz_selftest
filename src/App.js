ad09b207bd3618a48a2262fc13bd1865fb63f126
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-09 19:24:18 +0200

    add log in / out btn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Quiz from './components/Quiz';
 
 export default function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <>
-            <Header />
+            <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
             <Quiz />
         </>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,36 +2,52 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
     header: {
         display: 'flex',
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: 'space-around',
         backgroundColor: '#252d4a',
         height: 40,
+        width: '100%',
+    },
+    navLinks: {
+        fontSize: 20,
         fontWeight: 'bold',
         textTransform: 'uppercase',
-        fontSize: 20,
     },
     link: {
         margin: '0 16px',
         textDecoration: 'none',
     },
+    loginBtn: {
+        position: 'absolute',
+        right: '12%',
+    },
 });
 
-const Header = () => {
+const Header = ({ isLoggedIn, setIsLoggedIn }) => {
     const classes = useStyles();
 
     return (
         <AppBar position='static'>
             <Toolbar className={classes.header}>
-                <Link to='/' className={classes.link}>
-                    QUIZ
-                </Link>
-                <Link to='/results' className={classes.link}>
-                    Results
-                </Link>
+                <div className={classes.navLinks}>
+                    <Link to='/' className={classes.link}>
+                        QUIZ
+                    </Link>
+                    <Link to='/results' className={classes.link}>
+                        Results
+                    </Link>
+                </div>
+                <Button
+                    className={classes.loginBtn}
+                    onClick={() => setIsLoggedIn(!isLoggedIn)}
+                >
+                    {isLoggedIn ? 'Log In' : 'Log Out'}
+                </Button>
             </Toolbar>
         </AppBar>
     );
