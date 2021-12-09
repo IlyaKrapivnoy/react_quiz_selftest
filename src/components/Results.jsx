202fb759109d727ebc0d586205bae621a3a5f006
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-09 14:15:29 +0200

    change result page title

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from './Header';
 import ResultsTable from './ResultsTable';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        fontWeight: 'bold',
+        height: 80,
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 20px',
+    },
+}));
 
 const Test1 = () => {
+    const classes = useStyles();
+
     return (
         <>
             <Header />
-            <h1>RESULTS PAGE</h1>
+            <Typography variant='h4' className={classes.title}>
+                RESULTS PAGE
+            </Typography>
             <ResultsTable />
         </>
     );
